a46d2bf9590d929396a7631f4dffb398b53c352a
banhbio <ban@kuicr.kyoto-u.ac.jp>
2022-12-09 10:38:56 +0900

    fix scripts

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -2,29 +2,15 @@ import glob
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
-def tsv2table(tsv_paths):
-    tables = ''
-
-    for path in tsv_paths:
-        tables += '\n'
-        with open(path, 'r') as f:
-            title = f.readline() # read title
-            tables += title
-            
-            header_cols = f.readline().strip().split('\t') # read and split a table header
-            col_number = len(header_cols)
-            header_str = '|' + '|'.join(header_cols) + '|\n'
-            tables += header_str
-            
-            separater = "|:---" * col_number + '|\n'
-            tables += separater
-
-            for row in f.readlines():
-                tables += '|' + row.strip().replace('\t','|') +'|\n'
-            
-            tables += '\n'
-
-    return tables
+tables = [
+    ('ウイルス（ファージ）配列の予測(Prediction of viral and prophage sequences)', './Prediction.tsv'),
+    ('ゲノム(遺伝子)アノテーション(Genome/gene annotation tools)', './Annotation.tsv'),
+    ('配列のClassification (系統分類) Taxonomic classification of viral sequences', './Classifier.tsv'),
+    ('宿主予測 Host prediction', './Host_prediction.tsv'),
+    ('CRISPR同定 Identification of host CRISPR sequces', './CRISPR_detection.tsv'),
+    ('データベース Databases related to virology', './Database.tsv'),
+    ('プラスミド様配列の検出 Plasmid detection', './Plasmid_detection.tsv'),
+]
 
 if __name__ == '__main__':
 
@@ -35,16 +21,32 @@ if __name__ == '__main__':
         date = datetime.now(ZoneInfo('Asia/Tokyo'))
 
         tsv_paths = glob.glob('./*.tsv')
-        tables = tsv2table(tsv_paths)
 
+        is_tools_section = False
         for line in f.readlines():
-            is_tools_section = False
 
             if line == '# Tools\n':
                 is_tools_section = True
                 o.write(line)
                 o.write(f'<!-- The tables below were automatically generated at the {date} -->\n')
-                o.write(tables)
+                for t in tables:
+                    title, t_path = t
+                    o.write('\n')
+                    o.write(f'## {title}\n')
+
+                    with open(t_path, 'r') as t_f:
+                        header_cols = t_f.readline().strip().split('\t') # read and split a table header
+                        col_number = len(header_cols)
+                        header_str = '|' + '|'.join(header_cols) + '|\n'
+                        o.write(header_str)
+                        
+                        separater = "|:---" * col_number + '|\n'
+                        o.write(separater)
+
+                        for row in t_f.readlines():
+                            o.write('|' + row.strip().replace('\t', '|') + '|\n')
+                        
+                        o.write('\n')
             elif line.startswith('# '):
                 is_tools_section = False
             
